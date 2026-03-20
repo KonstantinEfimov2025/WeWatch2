0e3fd777e7aae9ad8814faecc16863a53d711d92
konstantin <konstantinefimov3742@gmail.com>
2026-03-20 12:17:56 +0700

    RetrofitClient

---
+++ app/src/main/java/com/example/wewatch/RetrofitClient.kt
@@ -0,0 +1,17 @@
+package com.example.wewatch
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    // Убедись, что используется https [cite: 3]
+    private const val BASE_URL = "https://www.omdbapi.com/"
+
+    val instance: MovieApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MovieApiService::class.java)
+    }
+}

608a9e38bd6fa2f46405aa73da1de45e83ea0774
konstantin <konstantinefimov3742@gmail.com>
2026-03-20 12:14:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/wewatch/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.wewatch
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.wewatch.ui.theme.WeWatchTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val database by lazy { MovieDatabase.getDatabase(this) }
+        val viewModel: MovieViewModel by viewModels {
+            MovieViewModelFactory(database.movieDao())
+        }
+
+        setContent {
+            WeWatchTheme {
+                WeWatchApp(viewModel)
+            }
+        }
+    }
+}
+
+@Composable
+fun WeWatchApp(viewModel: MovieViewModel) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "main") {
+        composable("main") {
+            MainScreen(
+                viewModel = viewModel,
+                onAddClick = { navController.navigate("search") }
+            )
+        }
+
+        composable("search") {
+            SearchScreen(
+                viewModel = viewModel,
+                onMovieSelected = { movie ->
+                    viewModel.selectMovie(movie)
+                    navController.navigate("add")
+                },
+                onBack = { navController.popBackStack() }
+            )
+        }
+
+        composable("add") {
+            AddScreen(
+                viewModel = viewModel,
+                onMovieAdded = {
+                    navController.navigate("main") {
+                        popUpTo("main") { inclusive = true }
+                    }
+                },
+                onBack = { navController.popBackStack() }
+            )
+        }
+    }
+}
